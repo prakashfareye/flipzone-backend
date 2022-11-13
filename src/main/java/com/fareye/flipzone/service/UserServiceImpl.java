59168687500daaf356799da2e30140c79151e919
prakash <prakash.ranjan1@getfareye.com>
2022-11-13 19:33:29 +0530

    minor changes in user service

--- src/main/java/com/fareye/flipzone/service/UserServiceImpl.java
+++ src/main/java/com/fareye/flipzone/service/UserServiceImpl.java
@@ -3,10 +3,12 @@ package com.fareye.flipzone.service;
 import com.fareye.flipzone.model.User;
 import com.fareye.flipzone.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.nio.file.FileSystemNotFoundException;
 import java.util.List;
 
+@Service
 public class UserServiceImpl implements UserService{
 
     @Autowired
