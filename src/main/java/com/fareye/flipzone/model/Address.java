f4d4818326454aa1f428b482a085256bc54382b1
ganjispandana30 <ganji.spandana@getfareye.com>
2022-11-16 15:17:37 +0530

    Builder annotation added

--- src/main/java/com/fareye/flipzone/model/Address.java
+++ src/main/java/com/fareye/flipzone/model/Address.java
@@ -1,12 +1,10 @@
 package com.fareye.flipzone.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 
+@Builder
 @Getter
 @Setter
 @Entity
