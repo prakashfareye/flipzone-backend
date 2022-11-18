dfcb69780498cd35d6bcf7183216f9c6d0a2379f
divyraj1 <divyraj1@getfareye.com>
2022-11-18 17:02:39 +0530

    fridayevening

--- src/main/java/com/fareye/flipzone/repository/OrderItemRepository.java
+++ src/main/java/com/fareye/flipzone/repository/OrderItemRepository.java
@@ -4,7 +4,11 @@ import com.fareye.flipzone.model.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderItemRepository extends JpaRepository<OrderItem,Long> {
+
+    List<OrderItem>findByOrder_OrderId(long id);
 
 }
